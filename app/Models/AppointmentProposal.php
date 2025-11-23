1f94d75ab14da9c8f9e23fa9666de92488109743
Torben Steinhauer <tsteinhauer@gmail.com>
2025-11-23 15:22:14 +0100

    initial commit

---
+++ app/Models/AppointmentProposal.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AppointmentProposal extends Model
+{
+    /** @use HasFactory<\Database\Factories\AppointmentProposalFactory> */
+    use HasFactory;
+}
